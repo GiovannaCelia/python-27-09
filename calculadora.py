8b486f772234e89a5b4aeb468610f8a080dd470c
Giovanna <146258903+GiovannaCelia@users.noreply.github.com>
2023-10-03 17:06:47 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,38 +7,51 @@ def selecao():
 
 def soma (prim,seg):
  soma= (prim+seg)
- print(soma)
- 
+ print (soma)
+ return soma
+
 def subtracao (prim,seg):
  subtracao= (prim-seg)
  print(subtracao)
-  
+ return subtracao
+ 
 def multiplicacao (prim,seg):
  multiplicacao= (prim*seg)
  print(multiplicacao)
+ return multiplicacao
  
 def divisao (prim,seg):
  divisao= (prim/seg)
- print(divisao)
+ print (divisao)
+ return divisao
 
 aux = 0 
+resultado=0
+vetor = []
 
-while ( aux <= 4):
+
+while ( aux <= 4 and aux <= 10):
     
     prim= float(input('Digite um número: '));
     seg= float(input('Digite um número: '));
     
     aux = selecao()
-    
-    
+
     if aux == 1 : 
-       soma(prim, seg)
+       resultado= soma(prim, seg)
     elif aux==2:
-      subtracao(prim, seg)   
+       resultado= subtracao(prim, seg)   
     elif aux ==3:
-      multiplicacao(prim,seg)
+      resultado= multiplicacao(prim,seg)
     elif aux == 4:
-        divisao(prim,seg)
+        resultado= divisao(prim,seg)
+    else :
+      print('Você saiu da calculadora')
+    vetor.append(resultado)
+    
+print(' vetor dos resultados: ')
+for i in vetor: 
+ print(i, " ")
     else :
         print('Você saiu da calculadora')
         
